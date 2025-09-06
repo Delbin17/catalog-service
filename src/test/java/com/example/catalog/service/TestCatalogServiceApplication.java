807db1d70dec144cebb223c6125bb41bc27469c5
Delbin <delbin2019@gmail.com>
2025-09-06 23:25:01 +0530

    catlog Service intial

--- src/test/java/com/example/catalog/service/TestCatalogServiceApplication.java
+++ src/test/java/com/example/catalog/service/TestCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestCatalogServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
